10d4d8e59247eca2dec2ccc2d6e2d0f63f9e5e27
Koyomi-Maka <angelics2.aa@gmail.com>
2017-07-11 18:40:37 +0900

    케어 서비스

--- Animalgram/src/main/java/com/animal/service/CareServiceImpl.java
+++ Animalgram/src/main/java/com/animal/service/CareServiceImpl.java
@@ -1,5 +1,69 @@
 package com.animal.service;
 
+import java.util.List;
+
+import javax.inject.Inject;
+
+import org.springframework.stereotype.Service;
+
+import com.animal.domain.CareVO;
+import com.animal.domain.MemberVO;
+import com.animal.domain.ProfessionalVO;
+import com.animal.persistence.CareDAO;
+
+@Service
 public class CareServiceImpl implements CareService {
 
+	@Inject
+	CareDAO dao;
+
+	@Override
+	public void insert(CareVO vo) {
+		dao.insert(vo);
+	}
+
+	@Override
+	public int selectAll() {
+		return 0;
+	}
+
+	@Override
+	public List<CareVO> selectTen() {
+		return dao.selectTen();
+	}
+
+	@Override
+	public List<MemberVO> selectProm() {
+		return dao.selectProm();	
+	}
+
+	@Override
+	public List<ProfessionalVO> selectProp() {
+		return dao.selectProp();
+	}
+
+	@Override
+	public CareVO selectCareInfoc(int cno) {
+		return dao.selectCareInfoc(cno);
+	}
+
+	@Override
+	public ProfessionalVO selectCareInfop(int cno) {
+		return dao.selectCareInfop(cno);
+	}
+
+	@Override
+	public MemberVO selectCareInfom(int cno) {
+		return dao.selectCareInfom(cno);
+	}
+
+	@Override
+	public void update(CareVO vo) {
+		dao.update(vo);
+	}
+
+	@Override
+	public void delete(int gno) {
+		dao.delete(gno);
+	}
 }
